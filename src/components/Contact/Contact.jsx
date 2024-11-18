6b696227142161f55ec5c0ecee17b071e76a1898
nehalkhaled19 <nehalkhaled62@gmail.com>
2024-11-18 21:31:28 +0200

    responsive for md screens

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,11 +4,12 @@ import ContactForm from './ContactForm';
 export default function Contact() {
     return (
         <div className="main-border pt-0">
-        <section id="contact" className="container flex">
-        <div className=" w-2/4 z-30 flex items-center ">
-          <img src={img} alt="girl writes email" className="w-3/4 m-auto" />
+        <section id="contact" className="container lg:flex">
+        <div className=" lg:w-2/4 z-30 flex items-center ">
+        
+          <img src={img} alt="girl writes email" className="md:w-4/6 lg:w-3/4 m-auto" />
           </div>
-          <div className="w-2/4 px-10">
+          <div className="lg:w-2/4 px-10">
             <h2 className="main-header class-after">
               <span> <i class="icon-color fa-solid line-h fa-phone fa-xs"></i> Get </span>in Touch
             </h2>
